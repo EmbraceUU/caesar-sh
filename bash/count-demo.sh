1e690c3b860cd9504b32e6ad38a73cf2eb19c1dc
embrace u <26841787+EmbraceUU@users.noreply.github.com>
2019-04-24 17:10:36 +0800

    去掉一些敏感信息

--- bash/count-demo.sh
+++ bash/count-demo.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-HOST="10.10.10.10"
-USER="***"
+
+HOST="127.0.0.1"
+USER="root"
 PWD="***"
-NOW=`date -d'-7 days' '+%G-%m-%d %H:%M:%S'`
-MYSQL="mysql -h${HOST} -u${USER} -p${PWD} tradingcenter"
-ORDER_SQL="use etb_base; select uid, mobilephone  from nft_user where create_time > '${NOW}' ;"
-ORDER_COUNT_SQL_SEL="select a.user_id , a.exchange , a.symbol, b.mobilephone, sum(a.volume) s from trd_orders a left join etb_base.nft_user b on a.user_id = b.uid group by a.exchange, a.symbol order by mobilephone, exchange;"
+NOW=`date -d'-1 days' '+%G-%m-%d %H:%M:%S'`
+echo $NOW
+MYSQL="mysql -h${HOST} -u${USER} -p${PWD} "
+ORDER_SQL="use etb_base; select uid, mobilephone  from user where create_time > '${NOW}' ;"
+ORDER_COUNT_SQL_SEL="select a.user_id , a.exchange , a.symbol, b.mobilephone, sum(a.volume) s from orders a left join user b on a.user_id = b.uid group by a.exchange, a.symbol order by mobilephone, exchange;"
 MSG="[新增用户统计]<br>"
+MOBIPHONE_COMP="11111111"
 
 # 查询当日新增用户
 USER_ADD_DAY="$($MYSQL -N -e "$ORDER_SQL")"
@@ -24,9 +27,8 @@ do
 done
 MSG=$MSG"<br>"
 
+# 统计实盘交易
 MSG=$MSG"<br>[实盘交易用户统计]<br>"
-# 统计uid
-MSG1=$MSG1"手机号: ["$PHONE"]<br>"
 ORDERS_UID_COUNT="$($MYSQL -N -e "$ORDER_COUNT_SQL_SEL")"
 OLD_IFS=$IFS
 IFS=$'\n'
@@ -36,13 +38,14 @@ for data in "${ARR1[@]}"
 do
   eval $(echo "$data" | awk -F" " '{printf("USERID=%s\nEXCHANGE=%s\nSYMBOL=%s\nMOBIPHONE=%s\nVOLUME=%s",$1,$2,$3,$4,$5)}')
   RESULT=`grep $MOBIPHONE phone`
-  # 判断结果是否为空  结果为空时  输出数据
-  if [ -a "$RESULT" ]; then
-    # 如果与上一个mobi不同, 输出当前手机号  否则  不输出手机号 
-    # 输出数据
+  if [ -z "$RESULT" ]; then
+    if [ $MOBIPHONE_COMP != $MOBIPHONE ]; then
+      MSG=$MSG"<br>手机号: ["$MOBIPHONE"]<br>"
+      MOBIPHONE_COMP=$MOBIPHONE
+    fi
     echo $USERID" "$EXCHANGE" "$SYMBOL" "$MOBIPHONE" "$VOLUME
-    # MSG1=$MSG1$EXCHANGE" -- "$SYMBOL" -- "$VOLUME"<br>"
+    MSG=$MSG$EXCHANGE" -- "$SYMBOL" -- "$VOLUME"<br>"
   fi
 done
 
-# python /home/tools/smtp_count.py "当日新增用户统计" "$MSG"
+python smtp_count.py "当日新增用户统计" "$MSG"
